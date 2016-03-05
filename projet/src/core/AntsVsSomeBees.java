3a392c87b7a9e7e2ff10d038ba3373df52bdfad2
Quentin Tardivon <quentin.tardivon@telecomnancy.net>
2016-03-05 10:11:02 +0100

    Bodyguard est fini

--- projet/src/core/AntsVsSomeBees.java
+++ projet/src/core/AntsVsSomeBees.java
@@ -6,7 +6,7 @@ package core;
 public class AntsVsSomeBees {
 
 	public static void main (String[] args) {
-		AntColony colony = new AntColony(1, 8, 3, 200); // specify the colony ]tunnels, length, moats, food]
+		AntColony colony = new AntColony(1, 8, 0, 200); // specify the colony ]tunnels, length, moats, food]
 		Hive hive = Hive.makeInsaneHive(); // specify the attackers (the hive)
 		new AntGame(colony, hive); // launch the game
 	}
